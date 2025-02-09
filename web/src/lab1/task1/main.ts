8b1f7352ee57163c23832ff6e49f10627578a2b5
VeYand <ValerikLerik17@gmail.com>
2025-02-09 22:19:20 +0300

    Порефакторил 1-е задание

--- web/src/lab1/task1/main.ts
+++ web/src/lab1/task1/main.ts
@@ -1,68 +1,80 @@
 import {Letter} from './Letter/Letter'
 import {DrawStrategyB, DrawStrategyL, DrawStrategyTC} from './Letter/Strategy/DrawStrategy'
 
-const canvas = document.createElement('canvas')
-document.body.appendChild(canvas)
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-const ctx = canvas.getContext('2d')
-
-if (!ctx) {
-	throw new Error('Canvas 2D context not supported')
-}
-
-const amplitude = 50
-const period = 2000
-
 type LetterData = {
 	letter: Letter,
 	phase: number,
 }
 
-const letters: LetterData[] = [
-	{
-		letter: new Letter({x: 0, y: 0}, {r: 1, g: 0, b: 0}, new DrawStrategyTC()),
-		phase: 0,
-	},
-	{
-		letter: new Letter({x: 0, y: 0}, {r: 0, g: 1, b: 0}, new DrawStrategyB()),
-		phase: Math.PI / 3,
-	},
-	{
-		letter: new Letter({x: 0, y: 0}, {r: 0, g: 0, b: 1}, new DrawStrategyL()),
-		phase: (2 * Math.PI) / 3,
-	},
-]
+class App {
+	private readonly canvas: HTMLCanvasElement
+	private readonly ctx: CanvasRenderingContext2D
+	private readonly amplitude = 50
+	private readonly period = 2000
+	private readonly letterSpacing = 100
+	private readonly letters: LetterData[] = [
+		{
+			letter: new Letter({x: 0, y: 0}, {r: 1, g: 0, b: 0}, new DrawStrategyTC()),
+			phase: 0,
+		},
+		{
+			letter: new Letter({x: 0, y: 0}, {r: 0, g: 1, b: 0}, new DrawStrategyB()),
+			phase: Math.PI / 3,
+		},
+		{
+			letter: new Letter({x: 0, y: 0}, {r: 0, g: 0, b: 1}, new DrawStrategyL()),
+			phase: (2 * Math.PI) / 3,
+		},
+	]
 
-const resizeCanvas = () => {
-	canvas.width = window.innerWidth
-	canvas.height = window.innerHeight
-	render()
-}
+	constructor() {
+		this.canvas = document.createElement('canvas')
+		document.body.appendChild(this.canvas)
+		this.canvas.width = window.innerWidth
+		this.canvas.height = window.innerHeight
+		const ctx = this.canvas.getContext('2d')
 
-const render = () => {
-	ctx.clearRect(0, 0, canvas.width, canvas.height)
+		if (!ctx) {
+			throw new Error('Canvas 2D context not supported')
+		}
 
-	const centerX = canvas.width / 2
-	const centerY = canvas.height / 2
+		this.ctx = ctx
 
-	const time = Date.now()
+		window.addEventListener('resize', this.resizeCanvas)
+	}
 
-	letters.forEach((letterData, index) => {
-		const {letter, phase} = letterData
-		const offsetY = amplitude * Math.sin(2 * Math.PI * time / period + phase)
-		letter.setPosition({
-			x: centerX + (index - 1) * 100,
-			y: centerY + offsetY,
+	run = () => {
+		this.render()
+	}
+
+	private resizeCanvas = () => {
+		this.canvas.width = window.innerWidth
+		this.canvas.height = window.innerHeight
+		this.render()
+	}
+
+	private render = () => {
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+		const centerX = this.canvas.width / 2
+		const centerY = this.canvas.height / 2
+
+		const time = Date.now()
+
+		this.letters.forEach((letterData, index) => {
+			const {letter, phase} = letterData
+			const offsetY = this.amplitude * Math.sin((2 * Math.PI * time) / this.period + phase)
+			letter.setPosition({
+				x: centerX + (index - 1) * this.letterSpacing,
+				y: centerY + offsetY,
+			})
+
+			letter.draw(this.ctx)
 		})
 
-		letter.draw(ctx)
-	})
-
-	requestAnimationFrame(render)
+		requestAnimationFrame(this.render)
+	}
 }
 
-render()
-window.addEventListener('resize', resizeCanvas)
-
-export {}
+const app = new App()
+app.run()
