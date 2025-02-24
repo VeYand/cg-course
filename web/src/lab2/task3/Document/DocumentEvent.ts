567432ada0677603ffd536dce540bc1f4fd1d270
VeYand <ValerikLerik17@gmail.com>
2025-02-24 21:20:48 +0300

    Накидал алхимию

---
+++ web/src/lab2/task3/Document/DocumentEvent.ts
@@ -0,0 +1,20 @@
+type GameEvent =
+	| {type: 'NEW_ELEMENT_CREATED', data: NewElementCreatedPayload}
+	| {type: 'STUDIED_ELEMENT_CREATED', data: StudiedElementCreatedPayload}
+	| {type: 'INVALID_COMBINATION', data: InvalidCombinationPayload}
+
+type NewElementCreatedPayload = {
+	newElementIds: string[],
+}
+
+type StudiedElementCreatedPayload = {
+	elementIds: string[],
+}
+
+type InvalidCombinationPayload = {
+	attemptedElements: [string, string],
+}
+
+export type {
+	GameEvent,
+}
---
+++ web/src/lab2/task3/main.ts
@@ -0,0 +1,9 @@
+import {GameDocument} from './Document/GameDocument'
+import {AlchemyAppView} from './View/AlchemyAppView'
+
+const appDocument = new GameDocument()
+const view = new AlchemyAppView(appDocument)
+document.querySelector('#root')?.appendChild(view.getComponent())
+
+
+export {}
