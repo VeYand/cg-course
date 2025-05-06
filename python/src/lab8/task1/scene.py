93e32e00557ee333e1a5dabb916878494d25739a
VeYand <ValerikLerik17@gmail.com>
2025-05-06 11:53:17 +0300

    Основа 8й лабы

---
+++ python/src/lab8/task1/scene.py
@@ -0,0 +1,22 @@
+from model import *
+
+
+class Scene:
+    def __init__(self, app):
+        self.app = app
+        self.objects = []
+        self.load()
+        # skybox
+        self.skybox = AdvancedSkyBox(app)
+
+    def add_object(self, obj):
+        self.objects.append(obj)
+
+    def load(self):
+        app = self.app
+        add = self.add_object
+
+        add(Ferret(app, pos=(30, 0, -10)))
+
+    def update(self):
+        ...
---
+++ python/src/lab8/task1/vao.py
@@ -0,0 +1,39 @@
+from vbo import VBO
+from shader_program import ShaderProgram
+
+
+class VAO:
+    def __init__(self, ctx):
+        self.ctx = ctx
+        self.vbo = VBO(ctx)
+        self.program = ShaderProgram(ctx)
+        self.vaos = dict()
+
+        # vaos
+        # ferret
+        self.vaos['ferret'] = self.get_vao(
+            program=self.program.programs['default'],
+            vbo=self.vbo.vbos['ferret'])
+
+        # shadow ferret
+        self.vaos['shadow_ferret'] = self.get_vao(
+            program=self.program.programs['shadow_map'],
+            vbo=self.vbo.vbos['ferret'])
+
+        # skybox vao
+        self.vaos['skybox'] = self.get_vao(
+            program=self.program.programs['skybox'],
+            vbo=self.vbo.vbos['skybox'])
+
+        # advanced_skybox vao
+        self.vaos['advanced_skybox'] = self.get_vao(
+            program=self.program.programs['advanced_skybox'],
+            vbo=self.vbo.vbos['advanced_skybox'])
+
+    def get_vao(self, program, vbo):
+        vao = self.ctx.vertex_array(program, [(vbo.vbo, vbo.format, *vbo.attribs)], skip_errors=True)
+        return vao
+
+    def destroy(self):
+        self.vbo.destroy()
+        self.program.destroy()
